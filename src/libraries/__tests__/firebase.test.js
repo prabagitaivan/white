ad6a29ce4f9e535cb3517eb94cbf3ab9561e0283
Prabagita Ivan <prabagita12@gmail.com>
2020-02-05 23:35:34 +0700

    chore: remove unused firebase measurement id

--- src/libraries/__tests__/firebase.test.js
+++ src/libraries/__tests__/firebase.test.js
@@ -20,8 +20,7 @@ describe('libraries firebase', () => {
         REACT_APP_FIREBASE_API_KEY: 'firebase-api-key',
         REACT_APP_FIREBASE_PROJECT_ID: 'firebase-project-id',
         REACT_APP_FIREBASE_SENDER_ID: 'firebase-sender-id',
-        REACT_APP_FIREBASE_APP_ID: 'firebase-app-id',
-        REACT_APP_FIREBASE_MEASUREMENT_ID: 'firebase-measurement-id'
+        REACT_APP_FIREBASE_APP_ID: 'firebase-app-id'
       })
 
       expect(firebase.initializeApp).toHaveBeenCalledWith({
@@ -31,8 +30,7 @@ describe('libraries firebase', () => {
         projectId: 'firebase-project-id',
         storageBucket: 'firebase-project-id.appspot.com',
         messagingSenderId: 'firebase-sender-id',
-        appId: 'firebase-app-id',
-        measurementId: 'G-firebase-measurement-id'
+        appId: 'firebase-app-id'
       })
     })
   })
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,8 +5,7 @@ process.env = {
   REACT_APP_FIREBASE_API_KEY: 'firebase-api-key',
   REACT_APP_FIREBASE_PROJECT_ID: 'firebase-project-id',
   REACT_APP_FIREBASE_SENDER_ID: 'firebase-sender-id',
-  REACT_APP_FIREBASE_APP_ID: 'firebase-app-id',
-  REACT_APP_FIREBASE_MEASUREMENT_ID: 'firebase-measurement-id'
+  REACT_APP_FIREBASE_APP_ID: 'firebase-app-id'
 }
 
 jest.mock('firebase/app', () => ({
