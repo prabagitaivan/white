a03be2dd090966c2ee4b589b168d50332c6526b5
Prabagita Ivan <prabagita12@gmail.com>
2020-02-16 16:42:09 +0700

    style: create generator name

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import { ThemeProvider } from '@material-ui/core/styles'
+import { StylesProvider, ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import './global.css'
 import theme from './theme'
+
+const generateClassName = (rule, styleSheet) => {
+  const prefix = styleSheet.options.classNamePrefix
+  const name = `${prefix}-${rule.key}`
+
+  if (prefix.match(/^Mui/)) return name
+  else return `White${name}`
+}
 
 export default class Styles extends Component {
   render () {
     const { children } = this.props
 
     return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </StylesProvider>
     )
   }
 }
